b446d2a5749e8f47cd38ab77da3c7a863f387471
gabymarin13 <ana.marin.chaves@ucreativa.com>
2021-12-13 18:13:29 -0600

    Tarea 3

---
+++ src/test/java/ApiTest/DummyRestApiExample.java
@@ -0,0 +1,76 @@
+package ApiTest;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.*;
+import org.testng.annotations.*;
+
+public class DummyRestApiExample {
+    RequestSpecification requestSpecification;
+    ResponseSpecification responseSpecification;
+
+    @BeforeTest
+    public void setUp(){
+        requestSpecification = new RequestSpecBuilder().
+                setBaseUri("http://dummy.restapiexample.com/api/v1").build();
+        responseSpecification = new ResponseSpecBuilder().
+                expectStatusCode(200).
+                expectContentType(ContentType.JSON).
+                build();
+    }
+
+    @Test
+    public void GetallEmployessReturns200Code(){
+        //initial structure before refactor with specifications
+        /*given().
+                when().get("http://dummy.restapiexample.com/api/v1/employees").
+                then().assertThat().
+                statusCode(200).log().body();*/
+        //refactor to use specifications
+        given().spec(requestSpecification).
+                get("employees").
+                then().spec(responseSpecification).
+                log().body();
+    }
+
+    @Test
+    public void GetOneEmpReturnsCorrectDataAndStatusCode200(){
+        //initial structure before refactor with specifications
+        /*given().
+                when().get("http://dummy.restapiexample.com/api/v1/employee/24").
+                then().assertThat().
+                body("data.employee_name", equalTo("Doris Wilder")).
+                and().
+                statusCode(200).
+                log().body();*/
+        //refactor to use specifications
+        given().
+                spec(requestSpecification).
+                get("employee/24").
+                then().
+                spec(responseSpecification).
+                assertThat().body("data.employee_name", equalTo("Doris Wilder")).
+                log().body();
+    }
+
+    @Test
+    public void CreateEmployee(){
+        given().
+                spec(requestSpecification.body("{\"name\":\"test\",\"salary\":\"123\",\"age\":\"23\"}")).
+                post("create").then().spec(responseSpecification).log().body();
+    }
+
+    @Test
+    public void DeleteEmployeeFromRecord(){
+        given().
+                spec(requestSpecification).delete("delete/11").
+                then().
+                spec(responseSpecification).
+                assertThat().body("message", equalTo("Successfully! Record has been deleted")).log().body();
+    }
+
+}
+
